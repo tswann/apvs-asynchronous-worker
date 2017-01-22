2176b142cac8c90a45d0a8b6aa415c31e1f29f49
Thomas Swann <ts.swann@gmail.com>
2017-01-22 18:07:31 +0000

    Add remaining claim doc metadata params

--- app/services/data/insert-claim-document-metadata.js
+++ app/services/data/insert-claim-document-metadata.js
@@ -1,9 +1,13 @@
 const config = require('../../../knexfile').asyncworker
 const knex = require('knex')(config)
 
-module.exports = function (claimDocumentId) {
+module.exports = function (claimDocumentId, isText, description, matchingExpenseLine, confidence) {
   var claimDocMetadata = {
     'ClaimDocumentId': claimDocumentId,
+    'IsText': isText,
+    'Description': description,
+    'MatchingExpenseLine': matchingExpenseLine,
+    'Confidence': confidence
   }
 
   return knex('IntSchema.ClaimDocumentMetadata').insert(claimDocMetadata)
