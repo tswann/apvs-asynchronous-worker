8d1dd7d98dc885661567116df7ff1ce6802676f2
Thomas Swann <ts.swann@gmail.com>
2017-01-22 17:43:35 +0000

    Fixed require statement

--- app/services/workers/check-document.js
+++ app/services/workers/check-document.js
@@ -1,7 +1,7 @@
 const getClaimDocuments = require('../data/get-claim-documents')
 const getClaimExpense = require('../data/get-claim-expense')
 const insertClaimDocumentMetadata = require('../data/insert-claim-document-metadata')
-const analyseImage = require('../cognitive-services')
+const analyseImage = require('../cognitive-services/analyse-image')
 const serviceEnum = require('../../constants/cognitive-services-enum')
 const logger = require('../log')
 
